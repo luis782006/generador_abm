0c762fef674b4a40831e3c6b561c58a9c8d9031e
luis782006 <luis782006@gmail.com>
2023-05-16 15:16:05 -0300

    Creacion de form para registrar atributos.

---
+++ Data/Clase.cs
@@ -0,0 +1,37 @@
+﻿//Importo la Clase con el enum para traer el tipo de dato
+using System;
+using static Generador_ABM.Data.EnumTipoDato;
+
+namespace Generador_ABM.Data
+{
+    public  class Clase
+    {
+        public string? NombreAtributo { get; set; }
+        //Dataype Enumerador
+        public DataType TipoDato { get; set; }
+        public bool AceptaNull { get; set; }
+        public string? ValorPorDefecto { get; set; }
+
+        public Clase() { }
+
+        public List<Clase> listaAtributos=new List<Clase>();
+        public void AgregarAtributo(string nombreAtributo,DataType tipoDatoSeleccionado,bool aceptaNull, string valorPorDefecto)
+        {
+            listaAtributos.Add(new Clase
+            {
+                NombreAtributo = nombreAtributo,
+                TipoDato = tipoDatoSeleccionado,
+                AceptaNull = aceptaNull,
+                ValorPorDefecto = valorPorDefecto
+            });
+
+            // Limpiar formulario
+            //nombreAtributo = string.Empty;
+            //tipoDatoSeleccionado = DataType.String;
+            //aceptaNull = false;
+            //valorPorDefecto = string.Empty;
+
+           
+        }
+    }
+}
---
+++ Data/EnumTipoDato.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Generador_ABM.Data
+{
+    public class EnumTipoDato
+    {
+        //Propiedades
+        public string Name { get; set; }
+        public DataType Type { get; set; }
+
+        //Constructor
+        public EnumTipoDato(string name, DataType type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        //Enumerador con tipos de datos
+        public enum DataType
+        {
+            String,
+            Int,
+            Double,
+            Bool,
+            Char,
+            DateTime,
+            Decimal,
+            Float,
+            Long,
+            Short,
+            Byte,
+            SByte,
+            UInt,
+            ULong,
+            UShort
+        }
+
+    }
+}
