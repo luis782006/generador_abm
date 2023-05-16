61f09d6f540fa9859dc9d401c98fd8c647e87227
luis782006 <luisfelipe782006@gmail.com>
2023-05-15 23:38:46 -0300

    Inciando Formulario de clase

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 using Generador_ABM.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-
+using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
