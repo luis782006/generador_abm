1edcc0e7e35839cec0cbae35f99bce9bac40bc29
luis782006 <luisfelipe782006@gmail.com>
2023-06-02 20:02:28 -0300

    Se corrige el campo valor por defecto.

--- Data/ManejoDeArchivo.cs
+++ Data/ManejoDeArchivo.cs
@@ -16,41 +16,45 @@ namespace Generador_ABM;
             
             nombreArchivo = $"{path}\\{NombreDeClase}.cs";
             string IsNulo = "";
-            string ValorXDefect = "";
-            using (StreamWriter writer = new StreamWriter(nombreArchivo))
+            string ValorXDefect = null;
+        using (StreamWriter writer = new StreamWriter(nombreArchivo))
+        {
+            try
             {
-              
-            writer.WriteLine($"public class {NombreDeClase}");
+                writer.WriteLine($"public class {NombreDeClase}");
                 writer.WriteLine("{");
                 foreach (var item in lstAtributos)
                 {
-                // control de tipos de datos. 
+                    // control de tipos de datos.
                     if (!item.AceptaNull)
                     {
-                       IsNulo = "?";
-                }
+                        IsNulo = "?";
+                    }
                     else
                     {
-                        if (item.ValorPorDefecto != ValorXDefect)
-                        {
-                        ValorXDefect = $"={item.ValorPorDefecto};";
+                        if (item.ValorPorDefecto != null)// if (item.ValorPorDefecto != ValorXDefect)
+                            {
+                            ValorXDefect = $"='{item.ValorPorDefecto}';";
                         }
-
-                        
                     }
-                writer.WriteLine($" public {item.TipoDato}{IsNulo} {item.NombreAtributo} {{ get; set; }}{ValorXDefect}");
-                ValorXDefect = "";
-                IsNulo = "";
+
+                    writer.WriteLine($" public {item.TipoDato}{IsNulo} {item.NombreAtributo} {{ get; set; }}{ValorXDefect}");
+                    ValorXDefect = "";
+                    IsNulo = "";
                 }
                 writer.WriteLine("}");
-
+            }
+            finally
+            {
                 writer.Close();
+                writer.Dispose();
                 
             }
-            //Abrir el explorador de la carpetas de windows
-            //string argumentos = nombreArchivo;
-            //Process.Start("explorer.exe", argumentos);           
         }
+        //Abrir el explorador de la carpetas de windows
+        //string argumentos = nombreArchivo;
+        //Process.Start("explorer.exe", argumentos);           
+    }
 
         public static string RutaDeArchivo()
         {
@@ -60,13 +64,23 @@ namespace Generador_ABM;
     public static void ActualizarArchivo(string[] test)
     {
         string path = RutaDeArchivo();
-        File.CreateText(path);
-        foreach (var item in test)
+        //File.CreateText(path);
+        using (StreamWriter writer = new StreamWriter(path))
         {
-        string appendText = item + Environment.NewLine;
+            foreach (var item in test)
+            {
+                writer.WriteLine(item);
+            }
             
-        File.AppendAllText(path, appendText);
+            //foreach (var item in test)
+            //{
+            //string appendText = item + Environment.NewLine;
+            
+            //File.AppendAllText(path, appendText);
 
+            //}
+           writer.Close();
         }
+            //File.Replace(path, path,null);
     }
 }
